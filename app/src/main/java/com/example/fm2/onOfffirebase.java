b557fad7da8d49de8eff879bcebd6c28592cc5d9
YUVRAJ06singh08deora <06yuvraj2001singh@gmail.com>
2022-04-16 10:25:30 +0530

    On button code added

---
+++ app/src/main/java/com/example/fm2/onOfffirebase.java
@@ -0,0 +1,9 @@
+package com.example.fm2;
+
+public class onOfffirebase {
+    private int onOffvalue;
+    public onOfffirebase()
+    {
+
+    }
+}
--- app/src/main/java/com/example/fm2/onOrOffActivity.java
+++ app/src/main/java/com/example/fm2/onOrOffActivity.java
@@ -1,28 +1,47 @@
 package com.example.fm2;
-
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Switch;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class onOrOffActivity extends AppCompatActivity {
-    ImageButton onButton;
+    // creating a variable for our
+// Firebase Database.
+    FirebaseDatabase firebaseDatabase;
+
+    // creating a variable for our Database
+    // Reference for Firebase.
+    DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_or_off);
-        onButton=(ImageButton) findViewById(R.id.onOff);
-        onButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onActivity();
+        Switch sw = (Switch) findViewById(R.id.onOffSwitch);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    System.out.println("Button is turned on");
+                    int servo_onvalue=180;
+                    databaseReference.setValue(servo_onvalue);
+                } else {
+                    System.out.println("Button is turned off"); // The toggle is disabled
+                    int servo_offvalue=0;
+                    databaseReference.setValue(servo_offvalue);
+                }
             }
         });
     }
-    public void onActivity(){
-        Intent intent = new Intent(this, onactivity.class);
-        startActivity(intent);
-    }
+
 }
