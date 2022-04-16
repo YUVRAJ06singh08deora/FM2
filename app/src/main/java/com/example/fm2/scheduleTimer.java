7b362a8f85dcae0155552cc5086c5a5e653cf6f5
YUVRAJ06singh08deora <06yuvraj2001singh@gmail.com>
2022-04-16 17:09:40 +0530

    Updated logos and alignments and settimer added to the data base

--- app/src/main/java/com/example/fm2/scheduleTimer.java
+++ app/src/main/java/com/example/fm2/scheduleTimer.java
@@ -3,19 +3,40 @@ package com.example.fm2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class scheduleTimer extends AppCompatActivity {
+    Button setTime;
     FirebaseDatabase rootnode;
 
     // creating a variable for our Database
     // Reference for Firebase.
+
     DatabaseReference refrence1,refrence2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_timer);
+        setTime = (Button) findViewById(R.id.setTime);
+        setTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText date = (EditText)findViewById(R.id.date);
+                String date1= date .getText().toString();
+                EditText timer= (EditText)findViewById(R.id.timer);
+                String timer1= timer.getText().toString();
+                rootnode=FirebaseDatabase.getInstance();
+                refrence1=rootnode.getReference("ScheduleTimer/Date");
+                refrence1.setValue(date1);
+                refrence2=rootnode.getReference("ScheduleTimer/ScheduleTime");
+                refrence2.setValue(timer1);
+            }
+        });
+
     }
 }
